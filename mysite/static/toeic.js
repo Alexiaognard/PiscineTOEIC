506129bdf693984b78d782e80ee741d29f14f1ad
Alexia <ognardalexiats4@gmail.com>
2020-01-14 13:09:23 +0100

    Création Timer

--- mysite/static/toeic.js
+++ mysite/static/toeic.js
@@ -1,35 +1,8 @@
-function timer(){
-    const start = Date.now();
-    var ptmp = new Date();
-
-    var t;
-    var timer_is_on = 0;
-    if (!timer_is_on) {
-        var c = 0;
-        timer_is_on = 1;
-
-        timedCount();
-        var fin = start + 1450
-      }
-
-
-    function timedCount() {
-      var tempRest = fin - Date.now()
-      if (tempRest > 0){
-        document.getElementById("txt").value = tempRest;
-
-      }
-      else {
-        document.getElementById("txt").value = "On est à 0";
-      }
-      t = setTimeout(timedCount, 1);
 
 
 
 
-    }
 
-}
 
 
 
