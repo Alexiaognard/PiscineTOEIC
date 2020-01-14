60eac6d9f6d5877fbd7ffc621fc7accb10613f51
Aaron <aaronlazaroo@gmail.com>
2020-01-14 18:43:09 +0100

    Ajout bdd site admin

--- mysite/toeic/admin.py
+++ mysite/toeic/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from toeic.models import Classe, Etudiant, Professeur, Sujet, PartieSujet
+
+admin.site.register(Classe)
+admin.site.register(Etudiant)
+admin.site.register(Professeur)
+admin.site.register(Sujet)
+admin.site.register(PartieSujet)
